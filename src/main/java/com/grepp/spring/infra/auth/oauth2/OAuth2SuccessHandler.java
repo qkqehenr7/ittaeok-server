5b150acdbc0abd9d741842227c2dd13c1ca80552
qkqehenr7 <qkqehenr37@gmail.com>
2025-08-15 21:00:01 +0900

    🔧 chore: 리다이렉트 uri 간소화

--- src/main/java/com/grepp/spring/infra/auth/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/grepp/spring/infra/auth/oauth2/OAuth2SuccessHandler.java
@@ -41,11 +41,11 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private String DEFAULT_REDIRECT_URL;
 
     // 허용 도메인
-    private final List<String> ALLOWED_DOMAINS = Arrays.asList(
-        frontServerDomainA,
-        backendServer,
-        "https://localhost:3000"
-    );
+//    private final List<String> ALLOWED_DOMAINS = Arrays.asList(
+//        frontServerDomainA,
+//        backendServer,
+//        "https://localhost:3000"
+//    );
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -65,60 +65,60 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("로그인 완료.");
 
         // 최종적으로 로그인 후 redirect 되는 url 결정
-        String targetUrl = determineTargetUrl(request, response, authentication);
+//        String targetUrl = determineTargetUrl(request, response, authentication);
+//
+//        // 인증 과정에서 사용한 임시 쿠키를 제거합니다. (redirect_uri 쿠키)
+//        cookieAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
 
-        // 인증 과정에서 사용한 임시 쿠키를 제거합니다. (redirect_uri 쿠키)
-        cookieAuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
-
-        getRedirectStrategy().sendRedirect(request,response,targetUrl);
+        getRedirectStrategy().sendRedirect(request,response,DEFAULT_REDIRECT_URL);
     }
 
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
-        Authentication authentication) {
-
-        // 'redirect_uri' 쿠키에서 uri를 가져오기
-        // redirect uri 지정하지 않을 경우. 추후 실제 배포 도메인으로 변경해야 함.
-        String redirectUri = CookieUtils.getCookie(request, CookieAuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
-            .map(Cookie::getValue)
-            .orElse(DEFAULT_REDIRECT_URL); // 쿠키에 uri 없으면 default 로 이동
-
-        // 해당 uri가 허용된 도메인인지 검증
-        if (StringUtils.hasText(redirectUri) && isAllowedUrl(redirectUri)) {
-            log.info("클라이언트 지정 redirect url :{}", redirectUri);
-            return redirectUri;
-        } else {
-            log.info("허용되지 않거나 유효하지 않은 redirect uri이 요청되었습니다. 기본 url로 리다이렉트 합니다.");
-            return DEFAULT_REDIRECT_URL;
-        }
-    }
-
-    // redirect uri 검증 메서드
-    private boolean isAllowedUrl(String url) {
-        try {
-            // 입력받은 url 문자열을 URI 객체로 파싱
-            URI uri = new URI(url);
-            String host = uri.getHost(); // 로컬 테스트를 위해 host의 포트번호가 달라도 허용합니다.
-            String scheme = uri.getScheme();
-
-            if (!("http".equalsIgnoreCase(scheme)|| "https".equalsIgnoreCase(scheme))) {
-                return false;
-            }
-
-            if (host == null) {
-                return false;
-            }
-
-            return ALLOWED_DOMAINS.stream()
-                .anyMatch(allowedDomain -> {
-                    try {
-                        URI allowedUri = new URI(allowedDomain);
-                        return allowedUri.getScheme().equalsIgnoreCase(scheme) &&allowedUri.getHost().equalsIgnoreCase(host);
-                    } catch (URISyntaxException e) {
-                        return false;
-                    }
-                });
-        } catch (URISyntaxException e) {
-            return false;
-        }
-    }
+//    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+//        Authentication authentication) {
+//
+//        // 'redirect_uri' 쿠키에서 uri를 가져오기
+//        // redirect uri 지정하지 않을 경우. 추후 실제 배포 도메인으로 변경해야 함.
+//        String redirectUri = CookieUtils.getCookie(request, CookieAuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
+//            .map(Cookie::getValue)
+//            .orElse(DEFAULT_REDIRECT_URL); // 쿠키에 uri 없으면 default 로 이동
+//
+//        // 해당 uri가 허용된 도메인인지 검증
+//        if (StringUtils.hasText(redirectUri) && isAllowedUrl(redirectUri)) {
+//            log.info("클라이언트 지정 redirect url :{}", redirectUri);
+//            return redirectUri;
+//        } else {
+//            log.info("허용되지 않거나 유효하지 않은 redirect uri이 요청되었습니다. 기본 url로 리다이렉트 합니다.");
+//            return DEFAULT_REDIRECT_URL;
+//        }
+//    }
+//
+//    // redirect uri 검증 메서드
+//    private boolean isAllowedUrl(String url) {
+//        try {
+//            // 입력받은 url 문자열을 URI 객체로 파싱
+//            URI uri = new URI(url);
+//            String host = uri.getHost(); // 로컬 테스트를 위해 host의 포트번호가 달라도 허용합니다.
+//            String scheme = uri.getScheme();
+//
+//            if (!("http".equalsIgnoreCase(scheme)|| "https".equalsIgnoreCase(scheme))) {
+//                return false;
+//            }
+//
+//            if (host == null) {
+//                return false;
+//            }
+//
+//            return ALLOWED_DOMAINS.stream()
+//                .anyMatch(allowedDomain -> {
+//                    try {
+//                        URI allowedUri = new URI(allowedDomain);
+//                        return allowedUri.getScheme().equalsIgnoreCase(scheme) &&allowedUri.getHost().equalsIgnoreCase(host);
+//                    } catch (URISyntaxException e) {
+//                        return false;
+//                    }
+//                });
+//        } catch (URISyntaxException e) {
+//            return false;
+//        }
+//    }
 }

7c190e0547fd8017ade79979339ccd0715d5cb01
qkqehenr37 <qkqehenr37@naver.com>
2026-02-24 00:44:29 +0900

    ♻️ refactor: Schedule 및 Location 엔티티 캡슐화 및 Setter 제거

--- src/main/java/com/grepp/spring/app/model/schedule/entity/Schedule.java
+++ src/main/java/com/grepp/spring/app/model/schedule/entity/Schedule.java
@@ -28,11 +28,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Entity
 @Table(name = "Schedules")
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -84,14 +82,50 @@ public class Schedule extends BaseEntity {
 
     // 일정이 삭제되면 ScheduleMember 도 같이 삭제
     @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ScheduleMember> scheduleMembers  = new ArrayList<>();
+    private List<ScheduleMember> scheduleMembers = new ArrayList<>();
 
     // 일정이 삭제되면 일정에 포함된 장소도 삭제
     @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Location> locations  = new ArrayList<>();
+    private List<Location> locations = new ArrayList<>();
 
     // 일정이 삭제되면 일정에 포함된 workspace 도 같이 삭제
     @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Workspace> workspaces = new ArrayList<>();
+
+    // --- Domain Logic Methods ---
+
+    public void updateDetails(LocalDateTime startTime, LocalDateTime endTime, ScheduleStatus status,
+            String scheduleName, String description, String location,
+            String specificLocation, Double specificLatitude, Double specificLongitude) {
+        if (startTime != null)
+            this.startTime = startTime;
+        if (endTime != null)
+            this.endTime = endTime;
+        if (status != null)
+            this.status = status;
+        if (scheduleName != null)
+            this.scheduleName = scheduleName;
+        if (description != null)
+            this.description = description;
+        if (location != null)
+            this.location = location;
+        if (specificLocation != null)
+            this.specificLocation = specificLocation;
+        if (specificLatitude != null)
+            this.specificLatitude = specificLatitude;
+        if (specificLongitude != null)
+            this.specificLongitude = specificLongitude;
+    }
+
+    public void updateMeetingPlatform(MeetingPlatform meetingPlatform, String platformUrl) {
+        if (meetingPlatform != null)
+            this.meetingPlatform = meetingPlatform;
+        if (platformUrl != null)
+            this.platformUrl = platformUrl;
+    }
+
+    public void determineLocation(String location) {
+        this.location = location;
+    }
 
 }
